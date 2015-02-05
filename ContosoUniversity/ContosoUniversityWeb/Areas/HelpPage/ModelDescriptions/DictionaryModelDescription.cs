6c7fcd074e33dfe42a2a7ec8f9bde543305e641a
noeldiazro <noeldiazro@gmail.com>
2015-02-05 20:42:47 +0000

    Estructura inicial del proyecto Web

---
+++ ContosoUniversity/ContosoUniversityWeb/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ContosoUniversityWeb.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ContosoUniversity/ContosoUniversityWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ContosoUniversityWeb.WebApiApplication" Language="C#" %>
